97fcbd09531da0fd15c58adbac7590be3d869da3
KnorpelSenf <shtrog@gmail.com>
2024-01-09 16:03:00 +0100

    set up structure

--- src/in_data.rs
+++ src/in_data.rs
@@ -1,3 +1,7 @@
+use std::env::args;
+use std::io::stdin;
+use std::io::BufRead;
+
 #[derive(Debug)]
 pub struct InputData {
     pub epsilon: f64,
@@ -16,4 +20,41 @@ pub struct Job {
     pub id: u32,
     pub processing_time: f64,
     pub resource_amount: f64,
+}
+
+pub fn parse() -> InputData {
+    // get the first 3 command line arguments, which are numbers and than read all input lines with read_line inside a for loop and save the result in an array
+    let (m, r, e) = (
+        args().nth(1).unwrap().parse::<u32>().unwrap(),
+        args().nth(2).unwrap().parse::<f64>().unwrap(),
+        args().nth(3).unwrap().parse::<f64>().unwrap(),
+    );
+    let mut jobs = Vec::new();
+    for line in stdin().lock().lines() {
+        let line = line.unwrap();
+        let mut line = line.split(",");
+        let (x, y) = (
+            line.next().unwrap().parse::<f64>().unwrap(),
+            line.next().unwrap().parse::<f64>().unwrap(),
+        );
+        jobs.push((x, y));
+    }
+
+    InputData {
+        epsilon: e,
+        instance: Instance {
+            machine_count: m,
+            resource_limit: r,
+            jobs: Box::from(
+                jobs.iter()
+                    .enumerate()
+                    .map(|(id, (p, r))| Job {
+                        id: id as u32,
+                        processing_time: *p,
+                        resource_amount: *r,
+                    })
+                    .collect::<Vec<Job>>(),
+            ),
+        },
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,19 @@
-use std::env::args;
-use std::io::stdin;
-use std::io::BufRead;
-
-use crate::in_data::InputData;
-use crate::in_data::Instance;
-use crate::in_data::Job;
+mod algo;
 mod in_data;
+mod pretty;
+
+use crate::algo::compute_schedule;
+use crate::in_data::parse;
+use crate::pretty::pretty;
 
 fn main() {
-    // get the first 3 command line arguments, which are numbers and than read all input lines with read_line inside a for loop and save the result in an array
-    let (m, r, e) = (
-        args().nth(1).unwrap().parse::<u32>().unwrap(),
-        args().nth(2).unwrap().parse::<f64>().unwrap(),
-        args().nth(3).unwrap().parse::<f64>().unwrap(),
-    );
-    let mut jobs = Vec::new();
-    for line in stdin().lock().lines() {
-        let line = line.unwrap();
-        let mut line = line.split(",");
-        let (x, y) = (
-            line.next().unwrap().parse::<f64>().unwrap(),
-            line.next().unwrap().parse::<f64>().unwrap(),
-        );
-        jobs.push((x, y));
-    }
+    let in_data = parse();
+    println!("INPUT: {:?}", in_data);
 
-    let in_data = InputData {
-        epsilon: e,
-        instance: Instance {
-            machine_count: m,
-            resource_limit: r,
-            jobs: Box::from(
-                jobs.iter()
-                    .enumerate()
-                    .map(|(id, (p, r))| Job {
-                        id: id as u32,
-                        processing_time: *p,
-                        resource_amount: *r,
-                    })
-                    .collect::<Vec<Job>>(),
-            ),
-        },
-    };
+    println!("Computing schedule");
+    let schedule = compute_schedule(in_data);
+    println!("{:?}", schedule);
 
-    println!("{:?}", in_data);
+    println!("Done, result is:");
+    println!("{}", pretty(schedule));
 }
