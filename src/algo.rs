172075f91ac2d1112d26d091854c9dc038a29236
KnorpelSenf <shtrog@gmail.com>
2024-01-09 18:10:20 +0100

    intermediate commit

--- src/algo.rs
+++ src/algo.rs
@@ -1,5 +1,22 @@
-use crate::in_data::InputData;
+#[derive(Debug)]
+pub struct InputData {
+    pub epsilon: f64,
+    pub instance: Instance,
+}
 
+#[derive(Debug)]
+pub struct Instance {
+    pub machine_count: u32,
+    pub resource_limit: f64,
+    pub jobs: Box<[Job]>,
+}
+
+#[derive(Debug)]
+pub struct Job {
+    pub id: u32,
+    pub processing_time: f64,
+    pub resource_amount: f64,
+}
 #[derive(Debug)]
 pub struct Schedule {
     pub mapping: Box<[JobPosition]>,
