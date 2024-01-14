8ae959866b990a636fbc5e76d1d954c551a27f0f
KnorpelSenf <shtrog@gmail.com>
2024-01-14 19:43:42 +0100

    learn to hate the borrow checker
    Co-authored-by: Karim Issa <e5kimol4ser@users.noreply.github.com>

--- src/algo.rs
+++ src/algo.rs
@@ -40,28 +40,95 @@ pub fn compute_schedule(in_data: InputData) -> Schedule {
                 resource_limit,
             },
     } = in_data;
+    let jobs = jobs.iter().collect::<Vec<&Job>>();
+
     if 1.0 / epsilon >= machine_count.into() {
         unimplemented!("second case");
     }
     let _n = jobs.len();
-    let epsilon_prime = epsilon / 5.0;
-    let _p_max = jobs
-        .iter()
-        .max_by(compare_resource_amount)
-        .expect("no jobs found");
-    let threshold = epsilon_prime * resource_limit;
-    let (narrow_jobs, mut wide_jobs) = jobs
-        .iter()
-        .partition::<Vec<_>, _>(|job| job.resource_amount < threshold);
-    wide_jobs.sort_by(compare_processing_time);
+    let (_epsilon_prime, epsilon_prime_squared, narrow_jobs, wide_jobs, _p_max) =
+        preprocessing(epsilon, resource_limit, jobs);
+
     println!("Wide {:?}", wide_jobs);
     println!("Narrow {:?}", narrow_jobs);
+    let p_w: f64 = wide_jobs.iter().map(|job| job.processing_time).sum();
+    let groups = linear_grouping(epsilon_prime_squared * p_w, &wide_jobs);
+    println!("Grouped into {} groups", groups.len());
+    for group in groups {
+        println!("{:?}", group);
+    }
     Schedule {
         mapping: Box::from(vec![]),
     }
 }
 
-fn compare_processing_time(job0: &&Job, job1: &&Job) -> Ordering {
+fn preprocessing<'a>(
+    epsilon: f64,
+    resource_limit: f64,
+    jobs: Vec<&'a Job>,
+) -> (f64, f64, Vec<&'a Job>, Vec<&'a Job>, f64) {
+    let epsilon_prime = epsilon / 5.0;
+    let epsilon_prime_squared = epsilon_prime * epsilon_prime;
+    let p_max = jobs
+        .iter()
+        .max_by(compare_processing_time)
+        .expect("no jobs found")
+        .processing_time;
+    let threshold = epsilon_prime * resource_limit;
+    let (narrow_jobs, mut wide_jobs) = jobs
+        .iter()
+        .partition::<Vec<_>, _>(|job| job.resource_amount < threshold);
+    wide_jobs.sort_by(compare_resource_amount);
+
+    (
+        epsilon_prime,
+        epsilon_prime_squared,
+        narrow_jobs,
+        wide_jobs,
+        p_max,
+    )
+}
+
+fn linear_grouping<'a>(step: f64, jobs: &Vec<&'a Job>) -> Vec<&'a mut Vec<Job>> {
+    if jobs.len() == 0 {
+        return vec![];
+    }
+    let mut job_ids = 0..;
+    let mut groups: Vec<&mut Vec<Job>> = vec![&mut vec![]];
+    let mut current_processing_time = 0.0f64;
+    for (i, job) in jobs.iter().enumerate() {
+        let current_group = groups.last().unwrap();
+        let mut remaining_processing_time = job.processing_time;
+        loop {
+            // Handle last iteration if the job fits entirely
+            if remaining_processing_time <= step {
+                let new_job = Job {
+                    id: job_ids.next().unwrap(),
+                    processing_time: remaining_processing_time,
+                    resource_amount: job.resource_amount,
+                };
+                current_group.push(new_job);
+                current_processing_time += new_job.processing_time;
+                break;
+            }
+
+            // Split off a bit of the job for the current group
+            let remaining_space = (i + 1) as f64 * step - current_processing_time;
+            let new_job = Job {
+                id: job_ids.next().unwrap(),
+                processing_time: remaining_space,
+                resource_amount: job.resource_amount,
+            };
+            current_processing_time += new_job.processing_time;
+            current_group.push(new_job);
+            remaining_processing_time -= step;
+            groups.push(&mut vec![]); // open new group for the remaining part of the job
+        }
+    }
+    groups
+}
+
+fn compare_processing_time(job0: &&&Job, job1: &&&Job) -> Ordering {
     job0.processing_time
         .partial_cmp(&job1.processing_time)
         .expect("invalid processing time")
