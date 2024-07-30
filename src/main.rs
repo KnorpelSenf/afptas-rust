b2f51822f1bea237864e82d6ebc2242e0a2ff428
KnorpelSenf <shtrog@gmail.com>
2024-07-30 17:18:48 +0200

    implement fast schedule printing

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ mod algo;
 mod in_data;
 mod pretty;
 
+use std::time::Instant;
+
 use crate::algo::{compute_schedule, Instance};
 use crate::in_data::parse;
-use crate::pretty::pretty;
+use crate::pretty::{display, pretty};
 
 fn main() {
     let instance = parse();
@@ -15,9 +17,20 @@ fn main() {
         resource_limit,
         ref jobs,
     } = instance;
-    println!("Scheduling {} jobs on {} machines with a resource limit of {} with epsilon={} close to OPT", jobs.len(), machine_count, resource_limit, epsilon);
+    let job_count = jobs.len();
+    println!("Scheduling {job_count} jobs on {machine_count} machines with a resource limit of {resource_limit} with epsilon={epsilon} close to OPT");
 
+    let start = Instant::now();
     let schedule = compute_schedule(instance);
+    let duration = start.elapsed();
 
-    println!("Done, result is:\n{}", pretty(schedule));
+    println!("Done in {:?}.", duration);
+    if job_count <= 1000 {
+        println!("Result is (prettified):");
+        println!("{}", pretty(schedule));
+    } else {
+        println!("Result is:");
+        println!("{}", display(schedule));
+    }
+    println!("{machine_count} machines.");
 }
